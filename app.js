687a3750719b10411b79454fde4f418cd6d1fd58
linkzao <fabiancabau@hotmail.com>
2015-04-01 23:55:40 -0300

    added dicebot

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var hellobot = require('./hellobot');
+var dicebot = require('./dicebot');
  
 var app = express();
 var port = process.env.PORT || 3000;
@@ -12,6 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', function (req, res) { res.status(200).send('Hello world!') });
 
 app.post('/hello', hellobot);
+app.post('/roll', dicebot);
+
  
 // error handler
 app.use(function (err, req, res, next) {
