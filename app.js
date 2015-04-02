b628821785bb90c3767fee499dedf7cba9c7f78f
linkzao <fabiancabau@hotmail.com>
2015-04-02 01:34:21 -0300

    fix rpgbot not being imported

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var hellobot = require('./hellobot');
 var dicebot = require('./dicebot');
+var rpgbot = require('./rpgbot');
  
 var app = express();
 var port = process.env.PORT || 3000;
