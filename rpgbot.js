96689c4bbaf853295d618a79a8e03f803430889b
Fabian Venturini Cabau <fabiancabau@hotmail.com>
2015-04-07 18:18:09 -0300

    Update rpgbot to add /rpg_hero_stats

--- rpgbot.js
+++ rpgbot.js
@@ -84,45 +84,6 @@ module.exports = function (req, res, next) {
         botPayload = createPayload(BOT_USERNAME, BOT_ICON_EMOJI, heroLayout(), channel_id);
 
     }
-    else if (req.body.command == '/roll') {
-      // default roll is 2d6
-      var matches;
-      var times = 2;
-      var die = 6;
-      var rolls = [];
-      var total = 0;
-
-      if (req.body.text) {
-        // parse roll type if specified
-        matches = req.body.text.match(/^(\d{1,2})d(\d{1,2})$/);
-
-        if (matches && matches[1] && matches[2]) {
-          times = matches[1];
-          die = matches[2];
-
-        } else {
-          // send error message back to user if input is bad
-          return res.status(200).send('<number>d<sides>');
-        }
-      }
-
-      // roll dice and sum
-      for (var i = 0; i < times; i++) {
-        var currentRoll = roll(1, die);
-        rolls.push(currentRoll);
-        total += currentRoll;
-      }
-
-      // write response message and add to payload
-      botPayload.text = req.body.user_name + ' rolled ' + times + 'd' + die + ':\n' +
-                        rolls.join(' + ') + ' = *' + total + '*';
-
-      botPayload.username = BOT_USERNAME;
-      botPayload.channel = req.body.channel_id;
-      botPayload.icon_emoji = BOT_ICON_EMOJI;
-
-  }
-
 
   send(botPayload, function (error, status, body) {
     if (error) {
