08ed250bf750145946743d9589367b95fb2aa25a
Fabian Venturini Cabau <fabiancabau@hotmail.com>
2015-04-08 18:54:59 -0300

    Fix module bug

--- rpgbot.js
+++ rpgbot.js
@@ -1,5 +1,4 @@
 var request = require('request');
-var tools = require('./tools').Tools;
 var rpg_list = require('./commands/rpg_list').rpg_list;
 var rpg_create = require('./commands/rpg_create').rpg_create;
 var rpg_hero_stats = require('./commands/rpg_hero_stats').rpg_hero_stats;
@@ -7,31 +6,19 @@ var rpg_hero_stats = require('./commands/rpg_hero_stats').rpg_hero_stats;
 
 module.exports = function (req, res, next) {
 
-
-    //var players = [];
     var botPayload = {};
-    var NICKNAME_POS = 0;
-    var CLASS_POS = 1;
-
-    var BOT_USERNAME = 'BoardGame Bot';
-    var BOT_ICON_EMOJI = ':game_die:';
-
-
-    var STATE_STATS_WAITING = 1;
-    var STATE_STATS_DONE = 2;
-
 
     /*
-     token=gIkuvaNzQIHg97ATvDxqgjtO
-     team_id=T0001
-     team_domain=example
-     channel_id=C2147483705
-     channel_name=test
-     user_id=U2147483697
-     user_name=Steve
-     command=/weather
-     text=94070
-     */
+    token=gIkuvaNzQIHg97ATvDxqgjtO
+    team_id=T0001
+    team_domain=example
+    channel_id=C2147483705
+    channel_name=test
+    user_id=U2147483697
+    user_name=Steve
+    command=/weather
+    text=94070
+    */
 
     if (req.body.text != 'error') {
 
@@ -44,7 +31,7 @@ module.exports = function (req, res, next) {
         }
         else if (req.body.command == '/rpg_list') {
 
-            //Implementar REDIS aqui pra não dar overhead no banco
+            //TODO: Implementar REDIS aqui pra não dar overhead no banco
             botPayload = rpg_list.run(req);
 
         }
@@ -68,30 +55,7 @@ module.exports = function (req, res, next) {
         }
     });
 
-}
-
-
-function roll(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function createPayload(bot_name, bot_emoji, text, channel_id, attachments) {
-    payload = {};
-
-    payload.username = bot_name;
-    payload.icon_emoji = bot_emoji;
-    payload.text = text;
-    payload.channel_id = channel_id;
-    payload.attachments = attachments;
-
-    return payload;
-}
-
-
-
-
-
-
+};
 
 function send(payload, callback) {
     var uri = 'https://hooks.slack.com/services/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';
